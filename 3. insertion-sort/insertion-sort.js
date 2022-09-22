9f9f05a1e00cc218c098d91a6908b7d6e04f938a
Emma <emmaku@protonmail.com>
2022-09-22 21:47:55 +0900

    add comments to insertion sort implementation

--- 3. insertion-sort/insertion-sort.js
+++ 3. insertion-sort/insertion-sort.js
@@ -1,16 +1,29 @@
-newArr = [5, 22, 55, 2, 45, 21, 4, 6, 1]
+newArr = [5, 22, 55, 2, 45, 21, 4, 6, 1];
 
 function insertionSort(arr) {
-    for (let i=1; i<arr.length; i++) {
-        let swapVal = arr[i]
-        let swapIdx = i
-        for (let j=i-1; j>=0 && swapVal < arr[j]; j--) {
-            arr[j+1] = arr[j]
-            swapIdx = j
-        }
-        arr[swapIdx] = swapVal
-        // console.log(arr)
+  // consider the first element is in the sorted subarray and iterate over the remaining elements
+  for (let i = 1; i < arr.length; i++) {
+    const currentNum = arr[i];
+    let currentIndex = i;
+
+    // loop over the sorted subarray
+    // if the current number is smaller than the element to its left, it's out of order
+    for (
+      let previousIndex = i - 1;
+      previousIndex >= 0 && currentNum < arr[previousIndex];
+      previousIndex--
+    ) {
+      // so, update the current index
+      currentIndex = previousIndex;
+      // and move the left element forward
+      arr[previousIndex + 1] = arr[previousIndex];
     }
+
+    // place the number at its sorted index
+    arr[currentIndex] = currentNum;
+  }
+
+  return arr;
 }
 
-insertionSort(newArr)
+console.log(insertionSort(newArr));
