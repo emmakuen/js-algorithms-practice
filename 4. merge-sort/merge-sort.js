fe950a2984f5a4b559804a14ab2f0b11158a3f06
Emma <emmaku@protonmail.com>
2022-09-23 22:50:58 +0900

    add comments to merge sort implementation

--- 4. merge-sort/merge-sort.js
+++ 4. merge-sort/merge-sort.js
@@ -1,38 +1,45 @@
 const mergeSort = (arr) => {
-    if (arr.length <= 1) return arr
-    
-    let leftArr = mergeSort(arr.slice(0, arr.length/2))
-    let rightArr = mergeSort(arr.slice(arr.length/2))
-    
-    return merge(leftArr, rightArr)
-}
+  // return the array if its length is zero or one (this is the base case that would break the recursion)
+  if (arr.length <= 1) return arr;
 
+  // recursively call current function on the left and right subarrays
+  const leftArr = mergeSort(arr.slice(0, arr.length / 2));
+  const rightArr = mergeSort(arr.slice(arr.length / 2));
 
-const merge = (sortedArr1, sortedArr2) => {
-    let mergedArr = []
-    let i = 0
-    let j = 0
-    while (i < sortedArr1.length && j < sortedArr2.length) {
-        if(sortedArr1[i] < sortedArr2[j]) {
-            mergedArr.push(sortedArr1[i])
-            i++
-        } else if (sortedArr1[i] > sortedArr2[j]) {
-            mergedArr.push(sortedArr2[j])
-            j++
-        } else {
-            mergedArr.push(sortedArr1[i], sortedArr2[j])
-            i++
-            j++
-        }
-        
+  // after the recursive calls, merge sorted arrays and return it
+  return merge(leftArr, rightArr);
+};
+
+const merge = (sortedArray1, sortedArray2) => {
+  let mergedArray = [];
+  let index1 = 0;
+  let index2 = 0;
+
+  while (index1 < sortedArray1.length && index2 < sortedArray2.length) {
+    // compare elements from two sorted arrays
+    // push whichever's smaller to the merged array
+    // and increment that array's index
+    if (sortedArray1[index1] < sortedArray2[index2]) {
+      mergedArray.push(sortedArray1[index1]);
+      index1++;
+    } else if (sortedArray1[index1] > sortedArray2[index2]) {
+      mergedArray.push(sortedArray2[index2]);
+      index2++;
+    } else {
+      // if two elements are equal, push both to the merged array and increment both indexes
+      mergedArray.push(sortedArray1[index1], sortedArray2[index2]);
+      index1++;
+      index2++;
     }
-    if (j < sortedArr2.length ) {
-        mergedArr = mergedArr.concat(sortedArr2.slice(j))
-    } else if (i < sortedArr1.length) {
-        mergedArr = mergedArr.concat(sortedArr1.slice(i))
-    }
-    
-    return mergedArr
-    
-}
+  }
 
+  // after the loop, one of the sorted arrays may have some elements left unmerged
+  // if so, merge the remaining elements to the merged array
+  if (index2 < sortedArray2.length) {
+    mergedArray = mergedArray.concat(sortedArray2.slice(index2));
+  } else if (index1 < sortedArray1.length) {
+    mergedArray = mergedArray.concat(sortedArray1.slice(index1));
+  }
+
+  return mergedArray;
+};
