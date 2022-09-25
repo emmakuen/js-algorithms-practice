466d782fc3e51444cf7600ca09d58f6548222536
Emma <emmaku@protonmail.com>
2022-09-25 18:59:14 +0900

    add comments to quick sort implementation

--- 5. quick-sort/quick-sort.js
+++ 5. quick-sort/quick-sort.js
@@ -1,21 +1,63 @@
-const testArr = [9, 5, 2, 1, 8, 4, 7, 6, 3]
+const testArr = [9, 5, 2, 1, 8, 4, 7, 6, 3];
 
-const quickSort = (arr) => {
-    let leftArr = []
-    let rightArr = []
-    if (arr.length <= 1) return arr
-    leftArr = quickSort(arr.slice(1).filter(num => num <= arr[0]))
-    rightArr = quickSort(arr.slice(1).filter(num => num > arr[0]))
-    let newPivotIdx = leftArr.length
-    arr[newPivotIdx] = arr[0]
-    for (let i=0; i < newPivotIdx; i++) {
-        arr[i] = leftArr[i]
-    }
-    for (let i=newPivotIdx + 1; i < arr.length; i++) {
-        arr[i] = rightArr[i-newPivotIdx-1]
-    }
-    console.log(arr)
-    return arr
-}
+const swap = (array, index1, index2) => {
+  // swap elements in place
+  [array[index1], array[index2]] = [array[index2], array[index1]];
+};
 
-quickSort(testArr)
+const partition = (array, startIndex, endIndex) => {
+  // take the element at start index as pivot
+  const pivot = array[startIndex];
+  // create two pointers
+  let left = startIndex;
+  let right = endIndex;
+
+  // loop over the elements from both sides until the pointers meet to find the sorted position of the pivot
+  // left elements should be less than the pivot
+  // right elements should be greater than the pivot
+
+  while (left < right) {
+    // keep moving the left pointer forward until the pointer encounters an element greater than the pivot
+    while (array[left] <= pivot) {
+      left++;
+    }
+
+    // keep moving the right pointer backwards until it encounters an element less than the pivot
+    while (array[right] > pivot) {
+      right--;
+    }
+
+    // if two pointers haven't met, swap left and right elements and move both pointers
+    if (left < right) {
+      swap(array, left, right);
+      left++;
+      right--;
+    }
+  }
+
+  // after the loop ends, two pointers would have met and right would be the sorted position of the pivot
+  // put the pivot in its correct index
+  swap(array, startIndex, right);
+
+  // return the pivot index
+  return right;
+};
+
+const quickSort = (arr, startIndex = 0, endIndex = arr.length - 1) => {
+  // if start index is less than the end index, don't make recursive calls (base case)
+  // else
+  if (startIndex < endIndex) {
+    // choose a pivot element and find its sorted position after placing elements to its right or left
+    // based on whether they're greater or less than the pivot
+    const pivotIndex = partition(arr, startIndex, endIndex);
+
+    // using the pivot element's position, divide array into two subarrays
+    // and call the quick sort recursively
+    quickSort(arr, startIndex, pivotIndex - 1);
+    quickSort(arr, pivotIndex + 1, endIndex);
+  }
+
+  return arr;
+};
+
+console.log(quickSort(testArr));
