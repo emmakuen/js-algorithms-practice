612f26a4568580a034a451ab9cb1820a378b4f79
Emma <emmaku@protonmail.com>
2022-09-27 21:02:52 +0900

    add comments to radix sort implementation

--- 6. radix-sort/radix-sort.js
+++ 6. radix-sort/radix-sort.js
@@ -1,24 +1,54 @@
-const myArr = [1556, 4, 3556, 593, 408, 4386, 902, 7, 8157, 86, 9637, 29]
+const nums = [1556, 4, 3556, 593, 408, 4386, 902, 7, 8157, 86, 9637, 29];
 
-const getDigit = (num, n) => {
-    return Math.floor(Math.abs(num) / Math.pow(10, n)) % 10
-}
+// --- HELPERS
+const getDigit = (num, digit) => {
+  return Math.floor(Math.abs(num) / Math.pow(10, digit)) % 10;
+};
 
+const countDigits = (num) => {
+  if (num === 0) return 1;
+
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+};
+
+const findMaxDigits = (nums) => {
+  let maxDigits = 0;
+  for (let i = 0; i < nums.length; i++) {
+    // count digits of each number and compare it to the current max digits
+    // update max digits count if the current number has more digits
+    maxDigits = Math.max(maxDigits, countDigits(nums[i]));
+  }
+
+  return maxDigits;
+};
+
+// --- RADIX SORT IMPLEMENTATION
 const radixSort = (nums) => {
-    let digitBuckets = Array.from({length: 10}, () => [])
-    let digit = 0
-    while(digitBuckets[0].length < nums.length) {
-        digitBuckets = Array.from({length: 10}, () => [])
-        nums.forEach(num => {
-            digitBuckets[getDigit(num, digit)].push(num)
-        })
-        nums = [].concat(...digitBuckets)
-        digit += 1
+  // find the maximum digits count
+  const maxDigits = findMaxDigits(nums);
+
+  // iterate over all the digits of nums
+  for (let kthDigit = 0; kthDigit < maxDigits; kthDigit++) {
+    // on each iteration,
+    // create a new array with empty buckets representing each digit (10 buckets for digits 0 - 9)
+    let digitBuckets = Array.from({ length: 10 }, () => []);
+
+    // for each number in the nums array
+    for (let i = 0; i < nums.length; i++) {
+      const num = nums[i];
+      // find the kth digit from the num
+      const digit = getDigit(num, kthDigit);
+
+      // from the digit buckets, find the bucket whose index is equal to current digit
+      // push the num to that bucket -- this would sort nums based on the kth digit
+      digitBuckets[digit].push(num);
     }
 
-    return nums
-}
+    // after the loop finishes, replace nums array with a new array with the sorted numbers
+    nums = [].concat(...digitBuckets);
+  }
 
-console.log(radixSort(myArr))
+  return nums;
+};
 
-
+console.log(radixSort(nums));
