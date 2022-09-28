c9b07a6e564c25054a788a1bf4cb14f25e58a094
Emma <emmaku@protonmail.com>
2022-09-28 22:52:03 +0900

    update dijkstra's algorithm

--- 13. diskstra/dijkstras-algorithm.js
+++ 13. diskstra/dijkstras-algorithm.js
@@ -1,19 +1,70 @@
+class Node {
+  constructor(value, priority) {
+    this.value = value;
+    this.priority = priority;
+  }
+}
+
 class PriorityQueue {
   constructor() {
     this.values = [];
   }
-
   enqueue(value, priority) {
-    this.values.push({ value, priority });
-    this.sort();
+    let newNode = new Node(value, priority);
+    this.values.push(newNode);
+    this.bubbleUp();
   }
-
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (element.priority >= parent.priority) break;
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
   dequeue() {
-    return this.values.shift();
+    const min = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return min;
   }
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
 
-  sort() {
-    this.values.sort((a, b) => a.priority - b.priority);
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild.priority < element.priority) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild.priority < element.priority) ||
+          (swap !== null && rightChild.priority < leftChild.priority)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
   }
 }
 
@@ -21,72 +72,60 @@ class WeightedGraph {
   constructor() {
     this.adjacencyList = {};
   }
-
-  addVertex(v) {
-    if (!(v in this.adjacencyList)) this.adjacencyList[v] = [];
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
   }
-
-  addEdge(v1, v2, weight) {
-    if (!(v1 in this.adjacencyList) || !(v2 in this.adjacencyList)) {
-      return false;
-    }
-
-    const list1 = this.adjacencyList[v1];
-    const list2 = this.adjacencyList[v2];
-
-    if (!list1.includes(v2)) list1.push({ vertex: v2, weight });
-    if (!list2.includes(v1)) list2.push({ vertex: v1, weight });
-
-    return true;
+  addEdge(vertex1, vertex2, weight) {
+    this.adjacencyList[vertex1].push({ node: vertex2, weight });
+    this.adjacencyList[vertex2].push({ node: vertex1, weight });
   }
-
-  djikstra(startVertex, endVertex) {
-    const priorityQueue = new PriorityQueue();
-    const minDistanceFor = {};
-    const previousVertexFor = {};
-
-    // initial state
+  dijkstra(start, finish) {
+    const nodes = new PriorityQueue();
+    const distances = {};
+    const previous = {};
+    let path = []; //to return at end
+    let smallest;
+    //build up initial state
     for (let vertex in this.adjacencyList) {
-      if (vertex === startVertex) {
-        priorityQueue.enqueue(vertex, 0);
-        minDistanceFor[vertex] = 0;
+      if (vertex === start) {
+        distances[vertex] = 0;
+        nodes.enqueue(vertex, 0);
       } else {
-        minDistanceFor[vertex] = Infinity;
-        priorityQueue.enqueue(vertex, Infinity);
+        distances[vertex] = Infinity;
+        nodes.enqueue(vertex, Infinity);
       }
-      previousVertexFor[vertex] = null;
+      previous[vertex] = null;
     }
-
-    // loop over the queue as long as there's next vertex to visit
-    while (priorityQueue.values.length) {
-      let currentVertex = priorityQueue.dequeue().value;
-
-      if (currentVertex === endVertex) {
-        const path = [];
-        // the path has been found, now build the path and return it
-        while (currentVertex in minDistanceFor) {
-          path.push(currentVertex);
-          currentVertex = previousVertexFor[currentVertex];
+    // as long as there is something to visit
+    while (nodes.values.length) {
+      smallest = nodes.dequeue().value;
+      if (smallest === finish) {
+        //WE ARE DONE
+        //BUILD UP PATH TO RETURN AT END
+        while (previous[smallest]) {
+          path.push(smallest);
+          smallest = previous[smallest];
         }
-        return { path: path.reverse(), cost: minDistanceFor[endVertex] };
+        break;
       }
-
-      if (currentVertex || minDistanceFor[currentVertex] !== Infinity) {
-        for (const neighborNode of this.adjacencyList[currentVertex]) {
-          // calculate the distance to neighbor node from the min vertex
-          const distance = minDistanceFor[currentVertex] + neighborNode.weight;
-          const neighborVertex = neighborNode.vertex;
-
-          if (minDistanceFor[neighborVertex] > distance) {
-            // if it's smaller than currently stored minimum distance to neighbor vertex, update its min distance
-            minDistanceFor[neighborVertex] = distance;
-
-            // update its previous vertex and update its priority in the queue
-            previousVertexFor[neighborVertex] = currentVertex;
-            priorityQueue.enqueue(neighborVertex, distance);
+      if (smallest || distances[smallest] !== Infinity) {
+        for (let neighbor in this.adjacencyList[smallest]) {
+          //find neighboring node
+          let nextNode = this.adjacencyList[smallest][neighbor];
+          //calculate new distance to neighboring node
+          let candidate = distances[smallest] + nextNode.weight;
+          let nextNeighbor = nextNode.node;
+          if (candidate < distances[nextNeighbor]) {
+            //updating new smallest distance to neighbor
+            distances[nextNeighbor] = candidate;
+            //updating previous - How we got to neighbor
+            previous[nextNeighbor] = smallest;
+            //enqueue in priority queue with new priority
+            nodes.enqueue(nextNeighbor, candidate);
           }
         }
       }
     }
+    return path.concat(smallest).reverse();
   }
 }
