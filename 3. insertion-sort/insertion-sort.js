3341b200a36fb54febf7f7fb11123c74cf1a763d
Emma <emmaku@protonmail.com>
2022-09-28 11:40:16 +0900

    update code for better readability

--- 3. insertion-sort/insertion-sort.js
+++ 3. insertion-sort/insertion-sort.js
@@ -4,23 +4,23 @@ function insertionSort(arr) {
   // consider the first element is in the sorted subarray and iterate over the remaining elements
   for (let i = 1; i < arr.length; i++) {
     const currentNum = arr[i];
-    let currentIndex = i;
+    let currentNumIndex = i;
 
     // loop over the sorted subarray
     // if the current number is smaller than the element to its left, it's out of order
     for (
-      let previousIndex = i - 1;
-      previousIndex >= 0 && currentNum < arr[previousIndex];
-      previousIndex--
+      let previousNumIndex = i - 1;
+      previousNumIndex >= 0 && currentNum < arr[previousNumIndex];
+      previousNumIndex--
     ) {
       // so, update the current index
-      currentIndex = previousIndex;
+      currentNumIndex = previousNumIndex;
       // and move the left element forward
-      arr[previousIndex + 1] = arr[previousIndex];
+      arr[previousNumIndex + 1] = arr[previousNumIndex];
     }
 
     // place the number at its sorted index
-    arr[currentIndex] = currentNum;
+    arr[currentNumIndex] = currentNum;
   }
 
   return arr;
